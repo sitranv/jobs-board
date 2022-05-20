45d837ee488fe4c57c8f9d5965cb37402738b98b
sitranv <sitranhue@gmail.com>
2022-05-20 17:00:44 +0700

    1

--- src/main/java/com/jobs/sitran/JobBoardApplication.java
+++ src/main/java/com/jobs/sitran/JobBoardApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JobBoardApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(JobBoardApplication.class, args);
 	}
 
